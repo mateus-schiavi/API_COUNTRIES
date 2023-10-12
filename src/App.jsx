244742e8a40f2018c509b17bb814752793b653e4
Mateus Schiavi <mtsschiavi@gmail.com>
2023-10-12 13:05:42 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Filter from './Components/Filter'
+
+function App() {
+  return (
+    <div>
+      <h1>Filtro de Países</h1>
+      <Filter/>
+    </div>
+  )
+}
+
+export default App
