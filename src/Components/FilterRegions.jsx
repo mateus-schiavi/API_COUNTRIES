753b1fd363aaf1bced7fa7b00ab72011282a8925
Mateus Schiavi <mtsschiavi@gmail.com>
2023-10-12 18:12:48 -0300

    added filter

---
+++ src/Components/FilterRegions.jsx
@@ -0,0 +1,52 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+
+function FilterRegions() {
+  const [data, setData] = useState([]);
+  const [continentFilter, setContinentFilter] = useState("");
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get("https://restcountries.com/v3.1/all");
+        setData(response.data);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  const handleContinentChange = (event) => {
+    setContinentFilter(event.target.value); 
+  };
+
+  const filteredCountries = data.filter((country) =>
+    continentFilter ? country.region === continentFilter : true
+  );
+
+  return (
+    <div>
+      <label>
+        Filter by Continent:
+        <select onChange={handleContinentChange} value={continentFilter}>
+          <option value="">All</option>
+          <option value="Africa">Africa</option>
+          <option value="Americas">Americas</option>
+          <option value="Asia">Asia</option>
+          <option value="Europe">Europe</option>
+          <option value="Oceania">Oceania</option>
+        </select>
+      </label>
+
+      <ul>
+        {filteredCountries.map((country) => (
+          <li key={country.cca2}>{country.name.common}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default FilterRegions;
