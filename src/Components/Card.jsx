702a8e85fc4cb3073eb3fb01b67082a8e867ff22
nicky <nickylds1958@gmail.com>
2023-10-12 14:35:47 -0600

    nombrre

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -6,12 +6,11 @@ function Card({ data }) {
     <div className="card-container">
       {data.map((country, index) => (
         <div className="card" key={index}>
-          <img
-            src={country.flags.svg}
-            alt={country.name.common}
-          />
+          <img src={country.flags.svg} alt={country.name.common} />
           <div>
-            <div>{country.name.common}</div>
+            <div>
+              <strong>{country.name.common}</strong>
+            </div>
             <div>Population: {country.population}</div>
             <div>Region: {country.region}</div>
             <div>Capital: {country.capital}</div>
