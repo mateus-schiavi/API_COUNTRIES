4d25fa0e3db06cf55e4efed4553d5a6b35c1416c
mi hector <hectorlandaezopsu@gmail.com>
2023-10-12 12:24:27 -0500

    primer comentario

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,30 @@
 import React from 'react'
 import Filter from './Components/Filter'
+import { useState } from 'react'
 
 function App() {
+  const [data, setData] = useState(null)
+
+  const getData = async()=>{
+    const res = await fetch (
+      "https://restcountries.com/v3.1/all"
+    )
+    const datos = await res.json()
+    setData(datos)
+
+    return datos
+  }
+    getData()
+
   return (
     <div>
       <h1>Filtro de Países</h1>
       <Filter/>
+    <div>
+      {datos.map((pais)=>{
+        console.log(pais)
+      })}
+    </div>
     </div>
   )
 }
